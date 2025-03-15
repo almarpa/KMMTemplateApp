7893dbc92c4320803d8c150ab2d0961b1cecf2e0
almarpa <alex_marco7@outlook.es>
2025-03-15 20:15:53 +0100

    feat(anim): refactor modifier animations

--- shared/core/common/src/commonMain/kotlin/com/almarpa/kmmtemplateapp/core/common/extensions/ModifierExtensions.kt
+++ shared/core/common/src/commonMain/kotlin/com/almarpa/kmmtemplateapp/core/common/extensions/ModifierExtensions.kt
@@ -1,35 +1,30 @@
 package com.almarpa.kmmtemplateapp.core.common.extensions
 
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.core.FiniteAnimationSpec
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.rememberTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalInspectionMode
-
-@Composable
-fun <Modifier> Modifier.applyIfCurrentLocalInspectionMode(block: Modifier.() -> Unit) =
-    if (LocalInspectionMode.current) {
-        this.apply { block() }
-    } else {
-        this
-    }
-
-@Composable
-fun <Modifier> Modifier.applyIfNotCurrentLocalInspectionMode(block: @Composable Modifier.() -> Unit) =
-    if (!LocalInspectionMode.current) {
-        this.apply { block() }
-    } else {
-        this
-    }
+import com.almarpa.kmmtemplateapp.core.common.model.enums.AnimState
 
 fun Modifier.shimmerLoadingAnimation(
     isLoadingCompleted: Boolean = false,
@@ -72,4 +67,57 @@ fun Modifier.shimmerLoadingAnimation(
             )
         }
     }
+}
+
+@Composable
+@OptIn(ExperimentalSharedTransitionApi::class)
+fun SharedTransitionScope.modifierWithSharedElementTransition(
+    animatedVisibilityScope: AnimatedVisibilityScope,
+    state: SharedTransitionScope.SharedContentState,
+): Modifier =
+    if (!LocalInspectionMode.current) {
+        Modifier.sharedElement(
+            state = state,
+            animatedVisibilityScope = animatedVisibilityScope,
+        )
+    } else {
+        Modifier
+    }
+
+@Composable
+fun modifierWithLazyGridAnimationPreview(index: Int, columns: Int) =
+    if (!LocalInspectionMode.current) {
+        Modifier.applyAlphaScaleAnimation(index, columns)
+    } else {
+        Modifier
+    }
+
+@Composable
+fun Modifier.applyAlphaScaleAnimation(index: Int, columns: Int): Modifier {
+    val animation: FiniteAnimationSpec<Float> = tween(
+        durationMillis = 400,
+        delayMillis = index % columns * 50,
+        easing = LinearOutSlowInEasing
+    )
+    val transitionState = remember {
+        MutableTransitionState(AnimState.PLACING).apply { targetState = AnimState.PLACED }
+    }
+    val transition = rememberTransition(transitionState)
+    val alpha by transition.animateFloat(transitionSpec = { animation }, label = "") { state ->
+        when (state) {
+            AnimState.PLACING -> 0f
+            AnimState.PLACED -> 1f
+        }
+    }
+    val scale by transition.animateFloat(transitionSpec = { animation }, label = "") { state ->
+        when (state) {
+            AnimState.PLACING -> 0f
+            AnimState.PLACED -> 1f
+        }
+    }
+    return graphicsLayer(
+        alpha = alpha,
+        scaleX = scale,
+        scaleY = scale,
+    )
 }
---
+++ shared/core/common/src/commonMain/kotlin/com/almarpa/kmmtemplateapp/core/common/model/enums/AnimState.kt
@@ -0,0 +1,3 @@
+package com.almarpa.kmmtemplateapp.core.common.model.enums
+
+enum class AnimState { PLACING, PLACED }
